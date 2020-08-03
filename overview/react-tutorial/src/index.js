3211d4c667f3278ddcc97e28c8f35602f59df784
Ashawe <harshsaglani@gmail.com>
2020-08-03 22:48:00 +0530

    :construction: Class Components. Table Component

--- overview/react-tutorial/src/index.js
+++ overview/react-tutorial/src/index.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 import './index.css'
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello, React!</h1>
-            </div>
-        )
-    }
-}
 
 ReactDOM.render(<App />, document.getElementById('root'))
