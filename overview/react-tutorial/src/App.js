05fd5dce3eaa69e6eeaf55e932426a29c2cfeb3a
Ashawe <harshsaglani@gmail.com>
2020-08-03 23:08:38 +0530

    :construction: Props

--- overview/react-tutorial/src/App.js
+++ overview/react-tutorial/src/App.js
@@ -3,9 +3,28 @@ import Table from './Table'
 
 class App extends Component {
     render() {
+        const characters = [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Aspring actress',
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartender',
+            },
+        ]
+
         return (
             <div className="container">
-                <Table />
+                <Table characterData={characters} />
             </div>
         )
     }
