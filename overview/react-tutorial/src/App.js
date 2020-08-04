137ad38bfd72e44eb9a0cb500c631d9f75c88f99
Ashawe <harshsaglani@gmail.com>
2020-08-04 23:07:33 +0530

    :sparkles: Implement Submiting form data - Local State

--- overview/react-tutorial/src/App.js
+++ overview/react-tutorial/src/App.js
@@ -1,31 +1,15 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
     state = {
-        characters: [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-            },
-            {
-                name: 'Mac',
-                job: 'Bouncer',
-            },
-            {
-                name: 'Dee',
-                job: 'Aspring actress',
-            },
-            {
-                name: 'Dennis',
-                job: 'Bartender',
-            },
-        ],
+        characters: [],
     }
 
     removeCharacter = (index) => {
         const { characters } = this.state
-        
+
         // You must use this.setState() to modify an array. Simply applying a new value to this.state.property will not work.
         this.setState({
             characters: characters.filter((character, i) => {
@@ -34,12 +18,17 @@ class App extends Component {
         })
     }
 
+    handleSubmit = (character) => {
+        this.setState({ characters: [...this.state.characters, character] })
+    }
+
     // A class component must include render(), and the return can only return one parent element.
     render() {
         const { characters } = this.state
         return (
             <div className="container">
                 <Table characterData={characters} removeCharacter={this.removeCharacter} />
+                <Form handleSubmit={this.handleSubmit} />
             </div>
         )
     }
