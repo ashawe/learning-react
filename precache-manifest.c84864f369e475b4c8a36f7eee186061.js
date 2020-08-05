b4a4c3dc5e4e5c2dc9ad7c8d1d3718e14112248a
Ashawe <harshsaglani@gmail.com>
2020-08-05 13:29:40 +0530

    Updates

---
+++ precache-manifest.c84864f369e475b4c8a36f7eee186061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2a9d651ca38e1401570a23bd217e65",
+    "url": "/ashawe/learning-react/index.html"
+  },
+  {
+    "revision": "fee712e7ec40d3f3488b",
+    "url": "/ashawe/learning-react/static/css/main.53574754.chunk.css"
+  },
+  {
+    "revision": "f3a8912e9639c9ea3ccc",
+    "url": "/ashawe/learning-react/static/js/2.1bbe048d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ashawe/learning-react/static/js/2.1bbe048d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee712e7ec40d3f3488b",
+    "url": "/ashawe/learning-react/static/js/main.116d70a4.chunk.js"
+  },
+  {
+    "revision": "45bbe97b8c3c5828fba0",
+    "url": "/ashawe/learning-react/static/js/runtime-main.3c29c566.js"
+  }
+]);
